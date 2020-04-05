299f8238ed57635a3885f9edcaba5b7394d32123
ReezanVisram <59345266+ReezanVisram@users.noreply.github.com>
2020-04-05 14:53:52 -0400

    Added the creation of lines and their ratings

--- engine.py
+++ engine.py
@@ -7,6 +7,7 @@ forwardPointsPerGame = [-1]
 
 defenseRanking = [-1]
 
+goalieRanking = [-1]
 
 def getTeamNames(data):
     teamNames = []
@@ -23,7 +24,7 @@ def getPlayersList(teamNames, data):
                 playersList.append(Player(teamNames[x], i['playerName'], i['playerPosition'], i['stats']['gamesPlayed'], i['stats']['goals'], i['stats']['assists'], i['stats']['points'], i['stats']['plusMinus'], i['stats']['timeOnIcePerGame'], 0))
             except:
                 try:
-                    playersList.append(Goalie(teamNames[x], i['playerName'], i['playerPosition'], i['stats']['gamesPlayed'], i['stats']['wins'], i['stats']['losses'], i['stats']['savePercentage'], i['stats']['goalsAgainstAverage']))
+                    playersList.append(Goalie(teamNames[x], i['playerName'], i['playerPosition'], i['stats']['gamesPlayed'], i['stats']['wins'], i['stats']['losses'], i['stats']['savePercentage'], i['stats']['goalsAgainstAverage'], 0))
 
                 except:
                     continue
@@ -50,22 +51,28 @@ for i in teamNames:
 for i in range(len(teams)):
     for j in range(len(teams[i].roster)):
         if (teams[i].roster[j].position != 'G'):
-
             if (teams[i].roster[j].position != 'D'):
                 currPlayerPPG = (teams[i].roster[j].points / teams[i].roster[j].gamesPlayed)
                 forwardPointsPerGame.append(currPlayerPPG)
-            
             else:
                 currPlayerNormalizedPlusMinus = (teams[i].roster[j].plusMinus / teams[i].roster[j].gamesPlayed)
                 currPlayerPPG = (teams[i].roster[j].points / teams[i].roster[j].gamesPlayed)
 
                 defenseRanking.append(currPlayerNormalizedPlusMinus + currPlayerPPG)
+        else:
+            currGoalieAdjustedSV = (teams[i].roster[j].savePercentage / teams[i].roster[j].goalsAgainstAverage)
+
+            currGoalieWinPct = (teams[i].roster[j].wins / teams[i].roster[j].gamesPlayed)
+    
+            goalieRanking.append(currGoalieAdjustedSV + currGoalieWinPct)
 
 forwardPointsPerGame.sort()
 defenseRanking.sort()
+goalieRanking.sort()
 
 forwardPointsPerGame = list(dict.fromkeys(forwardPointsPerGame))
 defenseRanking = list(dict.fromkeys(defenseRanking))
+goalieRanking = list(dict.fromkeys(goalieRanking))
 
 baseOverall = 70
 for ppg in range(len(forwardPointsPerGame)):
@@ -79,18 +86,34 @@ for ppg in range(len(forwardPointsPerGame)):
 
                 baseOverall = 70
 
-                print(player.playerName, "is", player.overall)
-
 for defense in range(len(defenseRanking)):
     for player in playersList:
         if (player.position == 'D'):
             currPlayerRanking = (player.plusMinus / player.gamesPlayed) + (player.points / player.gamesPlayed)
             if (currPlayerRanking == defenseRanking[defense]):
-                nearestMultipleOf14 = 14 * round(defense / 14)
-                baseOverall += nearestMultipleOf14 / 14
+                nearestMultipleOf10 = 10 * round(defense / 10)
+                baseOverall += nearestMultipleOf10 / 10
 
                 player.overall = baseOverall
 
                 baseOverall = 70
 
-                print(player.playerName, "is", player.overall)
+for goalie in range(len(goalieRanking)):
+    for player in playersList:
+        if (player.position == 'G'):
+            currGoalieRankingValue = (player.savePercentage / player.goalsAgainstAverage) + (player.wins / player.gamesPlayed)
+
+            if (currGoalieRankingValue == goalieRanking[goalie]):
+                nearestMultipleOf3 = 3 * round(goalie / 3)
+                baseOverall += nearestMultipleOf3 / 3
+
+                player.overall = baseOverall
+
+                baseOverall = 70
+
+
+for i in teams:
+    i.createAllInfo()
+
+
+print(teams[0].forwardLines)
--- playerclasses.py
+++ playerclasses.py
@@ -3,6 +3,130 @@ class Team:
         self.name = name
         self.roster = roster
 
+    def findPlayerByPosition(self, playerList, position):
+        for i in playerList:
+            if (i.position == position):
+                return i
+
+        return playerList[0]
+
+    def createGoalieLines(self):
+        self.goalieLines = ['G', 'G']
+
+        self.goalies = [player for player in self.roster if player.position == 'G']
+
+        self.goalies.sort(key=lambda x: x.overall, reverse=True)
+
+        self.goalieLines[0] = self.goalies[0]
+        self.goalieLines[1] = self.goalies[1]
+
+    def createDefenseLines(self):
+        self.defenseLines = [['D', 'D'], ['D', 'D'], ['D', 'D']]
+
+        self.defensemen = [player for player in self.roster if player.position == 'D']
+
+        self.defensemen.sort(key=lambda x: x.overall)
+
+        for i in range(len(self.defenseLines)):
+            for j in range(2):
+                for defenseman in self.defensemen:
+                    self.defenseLines[i][j] = defenseman
+                self.defensemen.remove(defenseman)
+
+    def createForwardLines(self):
+        self.forwardLines = [['LW', 'C', 'RW'], ['LW', 'C', 'RW'], ['LW', 'C', 'RW'], ['LW', 'C', 'RW']]
+
+        self.forwards = [player for player in self.roster if player.position != 'D']
+
+        for player in self.forwards:
+            if (player.position == 'G'):
+                self.forwards.remove(player)
+
+        self.forwards.sort(key=lambda x: x.overall, reverse=True)
+
+        for i in range(len(self.forwardLines)):
+            for j in range(3):
+                self.currPlayer = self.findPlayerByPosition(self.forwards, self.forwardLines[i][j])
+
+                self.forwardLines[i][j] = self.currPlayer
+
+                self.forwards.remove(self.currPlayer)
+
+    def createLines(self):
+        self.createGoalieLines()
+        self.createDefenseLines()
+        self.createForwardLines()
+
+    def getOffenseOverall(self):
+        lineAverageOveralls = []
+        self.teamOffensiveOverall = 0
+        for i in range(len(self.forwardLines)):
+            currLineOverall = 0
+            for j in range(len(self.forwardLines[i])):
+                currLineOverall += self.forwardLines[i][j].overall
+
+            currLineAverage = currLineOverall / 3
+            lineAverageOveralls.append(currLineAverage)
+
+        for overall in range(len(lineAverageOveralls)):
+            if (overall == 0):
+                self.teamOffensiveOverall += (lineAverageOveralls[overall] * 0.4)
+
+            elif (overall == 1):
+                self.teamOffensiveOverall += (lineAverageOveralls[overall] * 0.3)
+
+            elif (overall == 2):
+                self.teamOffensiveOverall += (lineAverageOveralls[overall] * 0.2)
+
+            elif (overall == 3):
+                self.teamOffensiveOverall += (lineAverageOveralls[overall] * 0.1)
+
+        self.teamOffensiveOverall = round(self.teamOffensiveOverall)
+
+ 
+    def getDefensiveOverall(self):
+        lineAverageOveralls = []
+        self.teamDefensiveOverall = 0
+        for i in range(len(self.defenseLines)):
+            currLineOverall = 0
+            for j in range(len(self.defenseLines[i])):
+                currLineOverall += self.defenseLines[i][j].overall
+
+            currLineAverage = currLineOverall / 2
+            lineAverageOveralls.append(currLineAverage)
+        
+        for overall in range(len(lineAverageOveralls)):
+            if (overall == 0):
+                self.teamDefensiveOverall += (lineAverageOveralls[overall] * 0.4)
+
+            elif (overall == 1):
+                self.teamDefensiveOverall += (lineAverageOveralls[overall] * 0.3)
+
+            elif (overall == 2):
+                self.teamDefensiveOverall += (lineAverageOveralls[overall] * 0.3)
+
+
+        self.teamDefensiveOverall = round(self.teamDefensiveOverall)
+
+
+    def getGoalieOverall(self):
+        self.teamGoalieOverall = 0
+
+        self.teamGoalieOverall += self.goalieLines[0].overall * 0.7
+        self.teamGoalieOverall += self.goalieLines[1].overall * 0.3
+
+        self.teamGoalieOverall = round(self.teamGoalieOverall)
+
+    def getAllOveralls(self):
+        self.getOffenseOverall()
+        self.getDefensiveOverall()
+        self.getGoalieOverall
+
+    def createAllInfo(self):
+        self.createLines()
+        self.getAllOveralls()                        
+
+
 class Player:
     def __init__(self, teamName, playerName, position, gamesPlayed, goals, assists, points, plusMinus, timeOnIcePerGame, overall):
         self.teamName = teamName
@@ -17,7 +141,7 @@ class Player:
         self.overall = 0
 
 class Goalie:
-    def __init__(self, teamName, playerName, position, gamesPlayed, wins, losses, savePercentage, goalsAgainstAverage):
+    def __init__(self, teamName, playerName, position, gamesPlayed, wins, losses, savePercentage, goalsAgainstAverage, overall):
         self.teamName = teamName
         self.playerName = playerName
         self.position = position
@@ -26,9 +150,6 @@ class Goalie:
         self.losses = losses
         self.savePercentage = savePercentage
         self.goalsAgainstAverage = goalsAgainstAverage
+        self.overall = 0
 
-    def determinePlayStyle(self):
-        pass
-
-    def determineOverall(self):
-        self.overall = (self.wins / self.gamesPlayed) + (self.wins * self.savePercentage) - (self.goalsAgainstAverage * (self.losses / self.gamesPlayed)) + 30
+   
